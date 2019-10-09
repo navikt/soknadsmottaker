ea19623007b840ee52b6b6a9e0ca557bdd70bc1f
Johan Sjöblom <johan.sjoblom@squeed.com>
2019-10-09 17:29:48 +0200

    Extracting code into more clear steps

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/service/ArchiverService.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/service/ArchiverService.kt
@@ -9,11 +9,13 @@ class ArchiverService(private val kafkaSender: KafkaSender) {
 	private val logger = LoggerFactory.getLogger(javaClass)
 
 	fun archive(message: String) {
-		publishToKafka(message)
+		val archivalData = createKafkaMessage(message)
+		publishToKafka(archivalData)
 	}
 
-	private fun publishToKafka(message: String) {
-		val archivalData = ArchivalData(message.reversed().toUpperCase(), message)
+	private fun createKafkaMessage(message: String) = ArchivalData(message.reversed().toUpperCase(), message)
+
+	private fun publishToKafka(archivalData: ArchivalData) {
 		logger.info("Publishing to Kafka: $archivalData")
 		kafkaSender.publish("archival", "key", archivalData)
 	}
