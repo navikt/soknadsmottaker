4377811c35e00804275505dec71654f223d42c99
R156312 <nils-arne.ringstad@nav.no>
2019-10-11 13:39:38 +0200

    Endret for å kunne ta imot rest kall fra henvendlese med de data denne kan levere. Det neste for applikasjonen er å legge til swagger (se SD-939). Fikset en feil i datamodellen

--- src/main/kotlin/no/nav/soknad/arkivering/dto/ArchivalData.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/dto/ArchivalData.kt
@@ -3,7 +3,7 @@ package no.nav.soknad.arkivering.dto
 import org.joda.time.DateTime
 
 data class SoknadMottattDto (var henvendelsesId: String, var ettersendelsesId: String?, var personId: String, var tema: String
-														 , var innsendtDato: DateTime, var mottatteDokumenter: List<InnsendtDokumentDto>)
+														 , var innsendtDato: DateTime, var mottatteDokumenter: List<MottattDokumentDto>)
 
 data class MottattDokumentDto(var uuid: String, var skjemaNummer: String, var erAlternativRepresentasjon: Boolean, var erHovedSkjema: Boolean,
 															 var tittel: String?, var mimeType: String?, var filNavn: String?, var filStorrelse: Int?)
