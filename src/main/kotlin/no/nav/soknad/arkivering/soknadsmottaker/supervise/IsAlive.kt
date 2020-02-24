06a571a3feabaff0e44e6bc876eab5280dac8859
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-02-24 14:41:28 +0100

    Eksperimentering med opplesing av miljøparametre. Bruker natpryce til å lese inn miljøvariable fra ulike mulige kilder, properties, system, environment. Local.properties manglet. Fikset prometheus path, mindre endringer i i IsAlive, lagt til path til appdynamics og environment parameter for appres_cms

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/supervise/IsAlive.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/supervise/IsAlive.kt
@@ -2,21 +2,29 @@ package no.nav.soknad.arkivering.soknadsmottaker.supervise
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping(value = ["/internal"])
 class IsAlive() {
 	private val logger = LoggerFactory.getLogger(javaClass)
 
-	@GetMapping("/internal/isAlive")
+	@GetMapping("/isAlive")
 	fun isAlive(): String {
 		logger.info("isAlive kalt")
 		return "Ok"
 	}
 
-	@GetMapping("/internal/ping")
+	@GetMapping("/ping")
 	fun ping(): String {
 		return "pong"
+	}
+
+	@GetMapping("/isReady")
+	fun isReady(): String {
+		logger.info("isReady kalt")
+		return "Ready for actions"
 	}
 
 }
