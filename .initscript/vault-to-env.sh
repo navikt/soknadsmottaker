404d79df0d38764e99a417f031fade440e238647
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-05-20 14:29:12 +0200

    Configuration cleanup

--- .initscript/vault-to-env.sh
+++ .initscript/vault-to-env.sh
@@ -2,5 +2,5 @@
 
 if test -f /var/run/secrets/nais.io/serviceuser/password
 then
-    export  SRVSSOKNADSMOTTAKER_PASSWORD=$(cat /var/run/secrets/nais.io/serviceuser/password)
+    export  SOKNADSMOTTAKER_PASSWORD=$(cat /var/run/secrets/nais.io/serviceuser/password)
 fi
