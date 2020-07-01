a7add26b0c23b615da4bba63b62eda58adbbfe43
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-07-01 11:42:55 +0200

    Slettet ubrukte filer

--- api-klient/upload-jar.cmd
+++
@@ -1,3 +0,0 @@
-mvn install:install-file -Dfile= target/soknadsmottaker-api-klient-1-sources.jar -DgroupId=no.nav.soknad.arkivering.api     -DartifactId=soknadsmottaker-api-klient -Dversion=1 -Dpackaging=jar
-	
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=pom.xml
