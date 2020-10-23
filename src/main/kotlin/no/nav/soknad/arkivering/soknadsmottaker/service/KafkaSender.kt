27f55fdd40ee057bf716085e749b5747ea104c37
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-10-23 12:51:05 +0200

    Setting headers on Kafka messages

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/service/KafkaSender.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/service/KafkaSender.kt
@@ -1,13 +1,24 @@
 package no.nav.soknad.arkivering.soknadsmottaker.service
 
 import no.nav.soknad.arkivering.avroschemas.Soknadarkivschema
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeaders
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
 class KafkaSender(private val kafkaTemplate: KafkaTemplate<String, Soknadarkivschema>) {
 
-	fun publish(topic: String, key: String, data: Soknadarkivschema) {
-		kafkaTemplate.send(topic, key, data)
+	private val messageId = "MESSAGE_ID"
+
+	fun publish(topic: String, key: String, value: Soknadarkivschema) {
+
+		val producerRecord = ProducerRecord(topic, key, value)
+		val headers = RecordHeaders()
+		headers.add(messageId, UUID.randomUUID().toString().toByteArray())
+		headers.forEach { h -> producerRecord.headers().add(h) }
+
+		kafkaTemplate.send(producerRecord)
 	}
 }
