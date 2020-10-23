71fdda77f8acef7a53d56d1cbd33df42f9f5018c
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-10-23 14:26:08 +0200

    Cleaning up IsAlive

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/supervise/IsAlive.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/supervise/IsAlive.kt
@@ -1,6 +1,5 @@
 package no.nav.soknad.arkivering.soknadsmottaker.supervise
 
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,29 +7,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping(value = ["/internal"])
 class IsAlive {
-	private val logger = LoggerFactory.getLogger(javaClass)
-	private var oppstart = 0
 
 	@GetMapping("/isAlive")
-	fun isAlive(): String {
-		if (oppstart < 10) {
-			logger.info("isAlive kalt")
-			oppstart++
-		}
-		return "Ok"
-	}
+	fun isAlive() = "Ok"
 
 	@GetMapping("/ping")
-	fun ping(): String {
-		return "pong"
-	}
+	fun ping() = "pong"
 
 	@GetMapping("/isReady")
-	fun isReady(): String {
-		if (oppstart < 10) {
-			logger.debug("isReady kalt")
-			oppstart++
-		}
-		return "Ready for actions"
-	}
+	fun isReady() ="Ready for actions"
 }
