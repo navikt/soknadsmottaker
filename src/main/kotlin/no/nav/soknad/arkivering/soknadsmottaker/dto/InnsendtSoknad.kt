f9d69b28945769aa7f1a747d1fc81f89ba21e2c6
nils-arne <50109859+nils-arne@users.noreply.github.com>
2021-02-01 16:42:42 +0100

    SD-1317 - Innsending av søknad feilet i soknadsmottaker. Initialisering av felt med null verdier med default.

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/dto/InnsendtSoknad.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/dto/InnsendtSoknad.kt
@@ -5,8 +5,28 @@ import java.time.LocalDateTime
 data class SoknadInnsendtDto(val innsendingsId: String, val ettersendelse: Boolean, val personId: String, val tema: String,
 														 val innsendtDato: LocalDateTime, val innsendteDokumenter: List<InnsendtDokumentDto>)
 
-data class InnsendtDokumentDto(val skjemaNummer: String, val erHovedSkjema: Boolean?,
-															 val tittel: String?, val varianter: List<InnsendtVariantDto>)
+data class InnsendtDokumentDto private constructor(val skjemaNummer: String, val erHovedSkjema: Boolean? = false,
+															 val tittel: String? = "Ikke spesifisert", val varianter: List<InnsendtVariantDto>) {
+	companion object {
+		operator fun invoke(skjemaNummer: String, erHovedSkjema: Boolean?,
+												tittel: String?, varianter: List<InnsendtVariantDto>
+		) = InnsendtDokumentDto(skjemaNummer,
+			erHovedSkjema ?: false,
+			tittel ?: "Ikke spesifisert",
+			varianter
+		)
+	}
+}
 
-data class InnsendtVariantDto(val uuid: String, val mimeType: String?, val filNavn: String?,
-															val filStorrelse: String?, val variantformat: String, val filtype: String)
+data class InnsendtVariantDto private constructor (val uuid: String, val mimeType: String?, val filNavn: String?,
+															val filStorrelse: String?, val variantformat: String, val filtype: String) {
+	companion object {
+		operator fun invoke(uuid: String, mimeType: String?, filNavn: String?, filStorrelse: String?, variantformat: String, filtype: String
+		) = InnsendtVariantDto(uuid, mimeType,
+			filNavn ?: "Ikke spesifisert",
+			filStorrelse ?: "0",
+			variantformat, filtype
+		)
+	}
+}
+
