c980718e53e6aa159b46450018bc0af456be20c7
Johan Sjöblom <johan.sjoblom@squeed.com>
2021-08-10 18:11:13 +0200

    Cleaning up

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/config/ApplicationConfig.kt
@@ -7,18 +7,17 @@ import java.io.File
 
 private val defaultProperties = ConfigurationMap(
 	mapOf(
-		"APP_VERSION" to "",
-		"SOKNADSMOTTAKER_USERNAME" to "kafkaproducer",
-		"SOKNADSMOTTAKER_PASSWORD" to "",
+		"KAFKA_TOPIC" to "privat-soknadInnsendt-v1-teamsoknad",
+		"KAFKA_METRICS_TOPIC" to "privat-soknadInnsendt-metrics-v1-teamsoknad",
 		"SCHEMA_REGISTRY_URL" to "http://localhost:8081",
 		"KAFKA_BOOTSTRAP_SERVERS" to "localhost:29092",
-		"KAFKA_CLIENTID" to "kafkaproducer",
+		"SOKNADSMOTTAKER_USERNAME" to "kafkaproducer",
+		"SOKNADSMOTTAKER_PASSWORD" to "",
 		"KAFKA_SECURITY" to "",
 		"KAFKA_SECPROT" to "",
 		"KAFKA_SASLMEC" to "",
+
 		"APPLICATION_PROFILE" to "",
-		"KAFKA_TOPIC" to "privat-soknadInnsendt-v1-teamsoknad",
-		"KAFKA_METRICS_TOPIC" to "privat-soknadInnsendt-metrics-v1-teamsoknad",
 		"REST_HENVENDELSE" to "avsender",
 		"REST_PASSORD" to "password"
 	)
@@ -38,12 +37,10 @@ fun readFileAsText(fileName: String, default: String = "") = try { File(fileName
 data class AppConfiguration(val kafkaConfig: KafkaConfig = KafkaConfig(), val restConfig: RestConfig = RestConfig()) {
 	data class KafkaConfig(
 		val profiles: String = "APPLICATION_PROFILE".configProperty(),
-		val version: String = "APP_VERSION".configProperty(),
 		val username: String = readFileAsText("/var/run/secrets/nais.io/serviceuser/username", "SOKNADSMOTTAKER_USERNAME".configProperty()),
 		val password: String = readFileAsText("/var/run/secrets/nais.io/serviceuser/password", "SOKNADSMOTTAKER_PASSWORD".configProperty()),
 		val servers: String = readFileAsText("/var/run/secrets/nais.io/kv/kafkaBootstrapServers", "KAFKA_BOOTSTRAP_SERVERS".configProperty()),
 		val schemaRegistryUrl: String = "SCHEMA_REGISTRY_URL".configProperty(),
-		val clientId: String = readFileAsText("/var/run/secrets/nais.io/serviceuser/username", "KAFKA_CLIENTID".configProperty()),
 		val secure: String = "KAFKA_SECURITY".configProperty(),
 		val protocol: String = "KAFKA_SECPROT".configProperty(), // SASL_PLAINTEXT | SASL_SSL
 		val salsmec: String = "KAFKA_SASLMEC".configProperty(), // PLAIN
@@ -53,7 +50,6 @@ data class AppConfiguration(val kafkaConfig: KafkaConfig = KafkaConfig(), val re
 	)
 
 	data class RestConfig(
-		val profiles: String = "APPLICATION_PROFILE".configProperty(),
 		val user: String = readFileAsText("/var/run/secrets/nais.io/kv/restUser", "REST_HENVENDELSE".configProperty()),
 		val password: String = readFileAsText("/var/run/secrets/nais.io/kv/restPassword", "REST_PASSORD".configProperty())
 	)
