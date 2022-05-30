6e3f151f06bb002e9e9494cce3d2af57c48eb774
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-05-30 13:22:32 +0200

    Using latest OpenAPI version which now has SpringDoc support. Removing Springfox annotations

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/NotifyApiImpl.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/NotifyApiImpl.kt
@@ -1,8 +1,5 @@
 package no.nav.soknad.arkivering.soknadsmottaker.rest
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import no.nav.soknad.arkivering.soknadsmottaker.api.NotifyApi
 import no.nav.soknad.arkivering.soknadsmottaker.model.AddNotification
 import no.nav.soknad.arkivering.soknadsmottaker.model.SoknadRef
@@ -10,8 +7,6 @@ import no.nav.soknad.arkivering.soknadsmottaker.service.NotificationService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -19,20 +14,6 @@ class NotifyApiImpl(private val notificationService: NotificationService) : Noti
 	private val logger = LoggerFactory.getLogger(javaClass)
 	private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
-	/**
-	 * The following annotations are copied from [NotifyApi.newNotification].
-	 */
-	@Operation(
-		summary = "Message in order to publish to Kafka hosted on Aiven user notification - Beskjed or Oppgave",
-		operationId = "newNotification",
-		description = "Depending on input, a message or a task user notification with a link to continue work is to be published")
-	@ApiResponses(
-		value = [ApiResponse(responseCode = "200", description = "Successful operation")])
-	@RequestMapping(
-		method = [RequestMethod.POST],
-		value = ["/notify/new"],
-		consumes = ["application/json"]
-	)
 	override fun newNotification(addNotification: AddNotification): ResponseEntity<Unit> {
 		val soknadRef = addNotification.soknadRef
 		val key = soknadRef.innsendingId
@@ -42,20 +23,6 @@ class NotifyApiImpl(private val notificationService: NotificationService) : Noti
 		return ResponseEntity(HttpStatus.OK)
 	}
 
-	/**
-	 * The following annotations are copied from [NotifyApi.cancelNotification].
-	 */
-	@Operation(
-		summary = "Message in order to publish message in order to cancel an user notification",
-		operationId = "cancelNotification",
-		description = "After an application is sent in or deleted previous added user notification shall be canceled")
-	@ApiResponses(
-		value = [ApiResponse(responseCode = "200", description = "Successful operation")])
-	@RequestMapping(
-		method = [RequestMethod.POST],
-		value = ["/notify/done"],
-		consumes = ["application/json"]
-	)
 	override fun cancelNotification(soknadRef: SoknadRef): ResponseEntity<Unit> {
 		val key = soknadRef.innsendingId
 		log(key, "Request to publish done notification for", soknadRef)
--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/TestApi.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/TestApi.kt
@@ -1,15 +1,10 @@
 package no.nav.soknad.arkivering.soknadsmottaker.rest
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import no.nav.soknad.arkivering.soknadsmottaker.api.SoknadTestApi
 import no.nav.soknad.arkivering.soknadsmottaker.model.Soknad
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,20 +12,6 @@ class TestApi : SoknadTestApi {
 	private val logger = LoggerFactory.getLogger(javaClass)
 	private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
-	/**
-	 * The following annotations are copied from [SoknadTestApi.receiveTest].
-	 */
-	@Operation(
-		summary = "Test endpoint that does nothing",
-		operationId = "receiveTest",
-		description = "Endpoint used for testing that does nothing.")
-	@ApiResponses(
-		value = [ApiResponse(responseCode = "200", description = "Successful operation")])
-	@RequestMapping(
-		method = [RequestMethod.POST],
-		value = ["/soknad-test"],
-		consumes = ["application/json"]
-	)
 	override fun receiveTest(soknad: Soknad): ResponseEntity<Unit> {
 		val key = soknad.innsendingId
 		log(key, soknad)
