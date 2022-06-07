54e6369d9aca601a575e7c7290e7111f43010644
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-06-07 15:42:29 +0200

    Will compare and log differences between data from Sendsoknad and Henvendelse

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/TestApi.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/TestApi.kt
@@ -17,6 +17,8 @@ class TestApi : SoknadTestApi {
 	private val logger = LoggerFactory.getLogger(javaClass)
 	private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
+	private val seenValues = hashMapOf<String, Soknad>()
+
 	/**
 	 * The following annotations are copied from [SoknadTestApi.receiveTest].
 	 */
@@ -31,11 +33,33 @@ class TestApi : SoknadTestApi {
 		value = ["/soknad-test"],
 		consumes = ["application/json"]
 	)
-	override fun receiveTest(soknad: Soknad): ResponseEntity<Unit> {
+	override fun receiveTest(soknad: Soknad, xInnsendingId: String?): ResponseEntity<Unit> {
 		val key = soknad.innsendingId
 		log(key, soknad)
 
+		updateOrCompareValues(xInnsendingId, key, soknad)
+
 		return ResponseEntity(HttpStatus.OK)
+	}
+
+	@Synchronized
+	private fun updateOrCompareValues(xInnsendingId: String?, key: String, soknad: Soknad) {
+		if (xInnsendingId != null && xInnsendingId.startsWith("henvendelse_")) {
+			val id = xInnsendingId.replace("henvendelse_", "")
+			if (id in seenValues) {
+				val oldSoknad = seenValues[id]
+				if (soknad == oldSoknad) {
+					logger.info("$key: Soknader are equal")
+					seenValues.remove(id)
+				} else {
+					logger.warn("$key: Soknader differs!\nOld: $oldSoknad\nNew: $soknad")
+				}
+			} else {
+				logger.warn("$xInnsendingId: Never seen Soknad before\n$soknad")
+			}
+		} else {
+			seenValues[key] = soknad
+		}
 	}
 
 	private fun log(key: String, soknad: Soknad) {
