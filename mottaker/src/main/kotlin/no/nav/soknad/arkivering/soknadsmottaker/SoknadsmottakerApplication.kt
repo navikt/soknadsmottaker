2b912c404009847b9db84e5d339caec88f02a72f
Alexander Petrov <alexander.petrov@nav.no>
2022-06-23 12:37:58 +0200

    revert openapi version

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/SoknadsmottakerApplication.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/SoknadsmottakerApplication.kt
@@ -1,11 +1,8 @@
 package no.nav.soknad.arkivering.soknadsmottaker
 
-import org.openapitools.SpringDocConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
-@Import(SpringDocConfiguration::class)
 @SpringBootApplication
 class SoknadsmottakerApplication
 
