7abe523bdcfdf75b86f65128f2ad73ed35c913ed
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-06-23 13:07:20 +0200

    Revert "revert openapi version"
    This reverts commit 2b912c404009847b9db84e5d339caec88f02a72f.

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/SoknadsmottakerApplication.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/SoknadsmottakerApplication.kt
@@ -1,8 +1,11 @@
 package no.nav.soknad.arkivering.soknadsmottaker
 
+import org.openapitools.SpringDocConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
+@Import(SpringDocConfiguration::class)
 @SpringBootApplication
 class SoknadsmottakerApplication
 
