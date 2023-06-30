d85e67d3504ee3e1e5108039804231ebe24cc240
Morten Stulen <morten.elias.stulen@nav.no>
2023-06-30 13:31:16 +0200

    Oppdater pakker

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/SoknadsmottakerApplication.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/SoknadsmottakerApplication.kt
@@ -2,12 +2,13 @@ package no.nav.soknad.arkivering.soknadsmottaker
 
 import org.openapitools.SpringDocConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
 @Import(SpringDocConfiguration::class)
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 @ConfigurationPropertiesScan
 class SoknadsmottakerApplication
 
