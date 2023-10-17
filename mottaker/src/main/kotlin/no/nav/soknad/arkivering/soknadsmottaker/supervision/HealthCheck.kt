8f9ae63602388267edbb416e17a03aab2009030d
Morten Stulen <morten.elias.stulen@nav.no>
2023-10-17 10:09:38 +0200

    Add logLink

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/supervision/HealthCheck.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/supervision/HealthCheck.kt
@@ -4,12 +4,13 @@ import no.nav.security.token.support.core.api.Unprotected
 import no.nav.soknad.arkivering.soknadsmottaker.api.HealthApi
 import no.nav.soknad.arkivering.soknadsmottaker.model.ApplicationStatus
 import no.nav.soknad.arkivering.soknadsmottaker.model.ApplicationStatusType
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 
 @Controller
-class HealthCheck : HealthApi {
+class HealthCheck(@Value("\${status_log_url}") private val logUrl: String) : HealthApi {
 
 	@Unprotected
 	override fun isAlive() = ResponseEntity<Unit>(HttpStatus.OK)
@@ -23,7 +24,7 @@ class HealthCheck : HealthApi {
 	@Unprotected
 	override fun getStatus(): ResponseEntity<ApplicationStatus> {
 		return ResponseEntity(
-			ApplicationStatus(status = ApplicationStatusType.OK, description = "OK"),
+			ApplicationStatus(status = ApplicationStatusType.OK, description = "OK", logLink = logUrl),
 			HttpStatus.OK
 		)
 	}
