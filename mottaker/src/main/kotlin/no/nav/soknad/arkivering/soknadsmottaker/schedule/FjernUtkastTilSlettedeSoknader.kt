8a9a756a2e8e8d7ff9bbb3456b3c9a1c0894cf91
nilsarne <nils-arne.ringstad@nav.no>
2023-11-01 17:11:44 +0100

    Lagt til kode for å kjøre engangsjobb for å slette gamle notifikasjoner

--- mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/schedule/FjernUtkastTilSlettedeSoknader.kt
+++ mottaker/src/main/kotlin/no/nav/soknad/arkivering/soknadsmottaker/schedule/FjernUtkastTilSlettedeSoknader.kt
@@ -1,5 +1,7 @@
 package no.nav.soknad.arkivering.soknadsmottaker.schedule
 
+import com.google.gson.Gson
+import no.nav.soknad.arkivering.soknadsmottaker.dto.UserNotificationMessageDto
 import no.nav.soknad.arkivering.soknadsmottaker.model.SoknadRef
 import no.nav.soknad.arkivering.soknadsmottaker.service.NotificationService
 import org.slf4j.Logger
@@ -8,7 +10,9 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.io.File
 import java.time.OffsetDateTime
+import java.util.*
 
 @Service
 @EnableScheduling
@@ -18,18 +22,46 @@ class FjernUtkastTilSlettedeSoknader(
 ) {
 	val logger: Logger = LoggerFactory.getLogger(javaClass)
 
-	@Value("\${cron.fjernGamleUtkast}")
+	@Value("\${cron.fjernGamleUtkastInputFil}")
 	private var envInput: String? = null
 
 	@Scheduled(cron = "\${cron.fjernGamleUtkast}")
 	fun start() {
-		if (leaderSelectionUtility.isLeader()) {
-			val utkastIds = listOf("eb0ac392-9ba9-4e68-91f9-cdd7f9081c45")
+		val inputString: String? = envInput ?: System.getenv("fjernGamleUtkastInputFil") ?: System.getProperty("fjernGamleUtkastInputFil")
+		if (leaderSelectionUtility.isLeader() && inputString != null) {
 
-			utkastIds.forEach { notificationService.cancelNotification(it, SoknadRef(it, false, it, "", OffsetDateTime.now(), false)) }
+			val utkastIds = try {
+				getApplicationIdsFromFile("slett-utkast-dev.json")
+			} catch (ex: Exception) {
+				logger.warn("Feil ved henting av fil med liste med søknader der utkast notifikasjoner skal slettes", ex)
+				return
+			}
+
+			utkastIds.forEach { notificationService.publishDoneUtkastNotification(it) }
 		}
 
 	}
 
+	private fun getApplicationIdsFromFile(filePath: String): List<String> {
+		val jsonString = readeBytesFromFile(filePath)
 
+		val gson = Gson()
+		val input = gson.fromJson((jsonString), BehandlingIds::class.java)
+
+		return input.ids
+	}
+	fun readeBytesFromFile(filePath: String): String {
+		this::class.java.classLoader
+			.getResourceAsStream(filePath).use { inputStream ->
+				assert(inputStream != null)
+				val s: Scanner = Scanner(inputStream!!).useDelimiter("\\A")
+				val json = if (s.hasNext()) s.next() else ""
+				assert(json != "")
+				return json
+			}
+	}
+
+	data class BehandlingIds(
+		val ids: List<String>
+	)
 }
