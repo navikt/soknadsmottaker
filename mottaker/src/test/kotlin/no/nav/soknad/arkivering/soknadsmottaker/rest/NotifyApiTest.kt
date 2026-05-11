23dbc06c168336a61979d256150c9a19bb93f84b
nilsarne <nils-arne.ringstad@nav.no>
2026-05-11 15:10:34 +0200

    lagt til slettesEtterDato på Utkast. Lagt til tester og spesifisering av produsent for å kunne kjøre tester. Oppdatering etter PR og avklaring rundt oppførsel med minside teamet

--- mottaker/src/test/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/NotifyApiTest.kt
+++ mottaker/src/test/kotlin/no/nav/soknad/arkivering/soknadsmottaker/rest/NotifyApiTest.kt
@@ -116,7 +116,7 @@ class NotifyApiTest {
 		assertTrue (utkastMsg.contains(innsendingsId))
 		assertTrue (utkastMsg.contains("\"utkastId\":\"$innsendingsId\""))
 		assertTrue (utkastMsg.contains("\"ident\":\"${soknadRef.personId}\""))
-		assertTrue (utkastMsg.contains("\"slettesEtter\":\"${slettesEtter.toString()}\""), "Fant ikke forventet slettesEtter, fikk $utkastMsg")
+		assertTrue (utkastMsg.contains("\"slettesEtter\":\"${slettesEtter.toString()}\""), "Fant ikke forventet \"aktivFremTil\":\"$slettesEtterString\", i $oppgaveMsg")
 
 	}
 
@@ -170,7 +170,7 @@ class NotifyApiTest {
 		assertTrue (oppgaveMsg.contains("\"type\":\"oppgave\""))
 		assertTrue (oppgaveMsg.contains("\"varselId\":\"$innsendingsId\""))
 		assertTrue (oppgaveMsg.contains("\"ident\":\"${soknadRef.personId}\""))
-		assertTrue (oppgaveMsg.contains("\"aktivFremTil\":\"$slettesEtterString\""), "Fant ikke forventet slettesEtter, fikk $oppgaveMsg")
+		assertTrue (oppgaveMsg.contains("\"aktivFremTil\":\"$slettesEtterString\""), "Fant ikke forventet \"aktivFremTil\":\"$slettesEtterString\", i $oppgaveMsg")
 
 	}
 
